2fcea3f54eff47c368f3b177d98516b9f175eda4
Peter Nowak <pnowak5@uic.edu>
2015-07-09 17:27:36 -0500

    Made seperate bmp header file Pixel output buffer reads fine until a certain point then begins to cascade by 1

--- SobelTest/src/main.cpp
+++ SobelTest/src/main.cpp
@@ -2,6 +2,7 @@
 #include <fstream>
 #include <malloc.h>
 #include "SobelTrying.hpp"
+#include "bmp2rgb.hpp"
 
 using namespace std;
 
@@ -11,13 +12,18 @@ int main(int argc, char *argv[])
 
 
 	BITMAPINFOHEADER bitmapInfoHeader;
+	RGBTRIPLE *pixelmap;
+	RGBTRIPLE pixel;
 	unsigned char *bitmapData;
-	bitmapData = LoadBitmapFile(argv[1],&bitmapInfoHeader);
+	pixelmap = LoadBitmapFile(argv[1],&bitmapInfoHeader);
 	PrintHeaderInfo(&bitmapInfoHeader);
 
-	//for(int i = 0; i < bitmapInfoHeader.biSizeImage-1;i++)
-	//	printf("%d,",bitmapData[i]);
+	for(int i = 0; i < bitmapInfoHeader.biSizeImage/sizeof(RGBTRIPLE);i++)
+	{
+		pixel = pixelmap[i];
+		printf("R:%d , B:%d , G:%d\n",pixel.rgbtRed,pixel.rgbtBlue,pixel.rgbtGreen);
 
+	}
 
 	return 0;
 }
